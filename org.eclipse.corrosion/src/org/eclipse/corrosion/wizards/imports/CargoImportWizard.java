e868c2c4da7de28282eaedd406351de746554d6a
Sven Marquardt <sven2qhotmail.de>
2018-03-02 21:58:15 +0100

    Import class for Cargo Projects

---
+++ org.eclipse.corrosion/src/org/eclipse/corrosion/wizards/imports/CargoImportWizard.java
@@ -0,0 +1,22 @@
+package org.eclipse.corrosion.wizards.imports;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class CargoImportWizard extends Wizard implements IImportWizard {
+
+	@Override
+	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
