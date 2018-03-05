e70d5e0a7b8e5888857bfc2d3c5b3add3e5e2e0f
Sven Marquardt <sven2qhotmail.de>
2018-03-05 21:33:16 +0100

    Import Wizard
    Import Wizard implementation to import cargo projects into corrosion.

--- org.eclipse.corrosion/src/org/eclipse/corrosion/wizards/imports/CargoImportWizard.java
+++ org.eclipse.corrosion/src/org/eclipse/corrosion/wizards/imports/CargoImportWizard.java
@@ -1,22 +1,43 @@
 package org.eclipse.corrosion.wizards.imports;
 
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
 
 public class CargoImportWizard extends Wizard implements IImportWizard {
 
+	private IWizardPage cargoImportWizardPage;
+
+	public CargoImportWizard() {
+		super();
+		this.setNeedsProgressMonitor(true);
+	}
+
 	@Override
 	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
-		// TODO Auto-generated method stub
-
+		this.cargoImportWizardPage = new CargoImportWizardPage(selection);
+		this.setWindowTitle("Import Existing Cargo Project");
+		this.addPages();
 	}
 
 	@Override
 	public boolean performFinish() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		// TODO Auto-generated method stub
+		super.addPages();
+		this.addPage(this.cargoImportWizardPage);
 	}
 
 }
